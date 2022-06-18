1cdcd0a3156a9ddb2ade92f509bfedab05c75a65
rmenai <rmenai@users.noreply.github.com>
2022-06-18 15:31:22 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { defineNuxtConfig } from "nuxt";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  srcDir: "src/",
+  buildModules: ["@pinia/nuxt", "@vueuse/nuxt", "nuxt-windicss"],
+  modules: ["@nuxtjs/color-mode"],
+
+  colorMode: { classSuffix: "" },
+});
